7ef27b3e53216f60d445a8eaf5ad5bdfad1e165b
chkt <ck@christoph-kettelhoit.de>
2016-08-30 01:21:59 +0200

    Added Circle2

---
+++ source/Circle2.js
@@ -0,0 +1,69 @@
+export default class Circle2 {
+
+	/**
+	 * Returns an instance representing p and r
+	 * @constructor
+	 * @param {Vector2} p - The center point
+	 * @param {number} r - The radius
+	 * @param {Circle2} [target] - The target instance
+	 * @returns {Circle}
+	 */
+	static Define(p, r, target) {
+		return target === undefined ? new Circle2(p, r) : target.constructor.call(target, p, r);
+	}
+
+	/**
+	 * Returns an instance at p with area a
+	 * @constructor
+	 * @param {Vector2} p - The center point
+	 * @param {number} a - The area
+	 * @param {Circle2} [target] - The target instance
+	 * @returns {Circle2}
+	 */
+	static Area(p, a, target) {
+		const r = Math.sqrt(a / Math.PI);
+
+		return this.Define(p, r, target);
+	}
+
+
+	/**
+	 * Creates a new instance
+	 * @param {Vector2} p - The center point
+	 * @param {number} r - The radius
+	 */
+	constructor(p, r) {
+		/**
+		 * The center point
+		 * @type {Vector2}
+		 */
+		this.center = p;
+		/**
+		 * The radius
+		 * @type {number}
+		 */
+		this.radius = r;
+	}
+
+
+	/**
+	 * Redefines the instance
+	 * @param {Vector2} p - The center point
+	 * @param {Vector2} r - The radius
+	 * @returns {Circle}
+	 */
+	define(p, r) {
+		this.constructor.call(this, p, r);
+
+		return this;
+	}
+
+
+	/**
+	 * The area
+	 * @type number
+	 */
+	get area() {
+		return Math.PI * this.radius * this.radius;
+	}
+}
